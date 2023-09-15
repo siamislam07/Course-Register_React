63db7a86447d8cbb13442120348942d2bed48507
siamislam07 <siamislam1970@gmail.com>
2023-09-15 16:43:45 +0600

    cart section added, select button woring good, also added toast for not selecting double

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-no-undef */
 import { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
-
+import Swal from "sweetalert2";
 
 const Card = () => {
     const [allCourse, setAllCourse] = useState([])
+    const [selectedCourse, setSelectedCourse] = useState([])
 
 
     useEffect(() => {
@@ -13,35 +14,50 @@ const Card = () => {
             .then(data => setAllCourse(data));
     }, [])
 
-    console.log(allCourse);
+    const handleSelectCourse = (course) => {
+        const isExist = selectedCourse.find(item => item.id == course.id)
+        if (isExist) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Already Selected',
+                text: 'Select AnotherOne',
+                // footer: '<a href="">Why do I have this issue?</a>'
+            })
+        } else {
+            setSelectedCourse([...selectedCourse, course])
+
+        }
+
+    }
+
 
     return (
         <div className="container">
             <div className="card-cart-container flex">
                 <div className="card-container w-[1048px] flex  flex-wrap gap-5 mb-5 ">
                     {
-                        allCourse.map(course =>(
+                        allCourse.map(course => (
                             <div key={course.id} className="card w-[300px] h-[440px] border rounded-lg bg-white">
-                        <div className="card-img">
-                            <img src={course.img} className="mt-5 mx-auto" alt="" />
+                                <div className="card-img">
+                                    <img src={course.img} className="mt-5 mx-auto" alt="" />
 
-                        </div>
-                        <h2 className="text-[19px] mt-4 font-semibold">{course.title}</h2>
-                        <p className="mt-3 text-left ml-2 pl-4 pr-4;">{course.description}</p>
-                        <div className="flex justify-evenly mt-4 mb-5">
-                            <p className="font-medium text-lg">Price: {course.price}</p>
-                            <p className="font-medium text-lg">Credit: {course.credit} hr</p>
-                        </div>
-                        <button className="bottom-0">Select</button>
+                                </div>
+                                <h2 className="text-[19px] mt-4 font-semibold">{course.title}</h2>
+                                <p className="mt-3 text-left ml-2 pl-4 pr-4 ;">{course.description}</p>
+                                <div className="flex justify-evenly mt-4 mb-5">
+                                    <p className="font-medium text-lg">Price: {course.price}</p>
+                                    <p className="font-medium text-lg">Credit: {course.credit} hr</p>
+                                </div>
+                                <button onClick={() => handleSelectCourse(course)} className="hover:bg-blue-600">Select</button>
 
-                    </div>
+                            </div>
                         ))
                     }
                 </div>
 
 
                 <div className="cart">
-                    <h1>This is cart side</h1>
+                    <Cart selectedCourse={selectedCourse}></Cart>
                 </div>
             </div>
         </div>
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,28 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 
 
-const Cart = () => {
+const Cart = ({ selectedCourse, }) => {
+    console.log(selectedCourse);
     return (
-        <div>
-            
+        <div className="mt-5 h-auto bg-white rounded-xl p-3 pl-8">
+            <h1 className="text-3xl text-[#2F80ED;] mb-5 font-bold">Credit Hour Remaining 20 hr</h1>
+            <hr className="w-80 mx-auto mb-5" />
+            <h3 className="text-2xl text-start font-bold">Course Name :</h3>
+            <div className="mt-5 space-y-2">
+                {
+                    selectedCourse.map(course => (
+
+                        <li key={course.id} className="list-decimal text-start text-xl">{course.title}</li>
+
+                    ))
+                }
+            </div>
+            <hr className="w-80 mx-auto mb-5 mt-5" />
+            <p className="text-start mb-5 text-xl">Total Credit Hour : 13</p>
+            <hr className="w-80 mx-auto mb-5" />
+            <p className="text-start mb-5 text-xl">Total Price: 2222222 USD</p>
+
         </div>
     );
 };
